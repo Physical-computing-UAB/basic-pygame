942e2a4de7c098f5a7c125986b4559e9942d70d0
fernando <fernando@cvc.uab.es>
2020-07-05 12:48:48 +0200

    next

--- basic-pygame.py
+++ basic-pygame.py
@@ -19,6 +19,8 @@ class Player():
         self.rect= (x,y,width, height)
         self.vel = 1
 
+
+
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
@@ -63,4 +65,3 @@ def main():
         redrawWindow(win, p)
 
 main()
-
